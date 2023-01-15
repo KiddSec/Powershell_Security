9ce5c162f87c52dd83fab70f64093bd4afeb6654
KiddSec <aikiddju@gmail.com>
2023-01-15 02:19:49 -0500

    first commit

---
+++ SSL Hardening.ps1
@@ -0,0 +1,19 @@
+$tls10 = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client", "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server"
+#$tls11 = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\protocols\TLS 1.1\Client", "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\protocols\TLS 1.1\Server"
+#$tls12 = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\protocols\TLS 1.2\Client", "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\protocols\TLS 1.2\Server"
+#$ssl2 = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\protocols\SSL 2.0\Client", "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\protocols\SSL 2.0\Server"
+#$ssl3 = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\protocols\SSL 3.0\Client", "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\protocols\SSL 3.0\Server"
+
+$tls10check = ( $tls10 | Test-Path )
+#$tls11check = ( $tls11 | Test-Path )
+#$tls12check = ( $tls12 | Test-Path)
+#$ssl2check = ( $ssl2 | Test-Path )
+#$ssl3check = ( $ssl3 | Test-Path )
+
+if ($tls10check -eq $false) {New-Item $tls10 -force; continue}
+else {New-ItemProperty -Path $tls10 -name 'Enabled' -value '0' -Type 'DWORD'
+	New-ItemProperty -Path $tls10 -name 'DisabledByDefault' -value '1' -Type 'DWORD'}
+
+
+
+
